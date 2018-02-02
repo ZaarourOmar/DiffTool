0adf7bd79a6d5375bd5958abdc20ec9467be31bc
Omar Zaarour <omar.zaarour@adoxio.com>
2018-02-01 19:35:29 -0700

    initial commit

---
+++ diffplex-master/diffplex-master/Perf.DiffPlex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Perf.DiffPlex
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(@"DiffPlex Perf Tester");
+
+            new DiffPerfTester().Run();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ diffplex-master/diffplex-master/WebDiffer/Controllers/DiffController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DiffPlex.DiffBuilder;
+
+namespace WebDiffer.Controllers
+{
+    public class DiffController : Controller
+    {
+        private readonly ISideBySideDiffBuilder diffBuilder;
+
+        public DiffController(ISideBySideDiffBuilder bidiffBuilder)
+        {
+            diffBuilder = bidiffBuilder;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+        public IActionResult Diff(string oldText, string newText)
+        {
+            var model = diffBuilder.BuildDiffModel(oldText ?? string.Empty, newText ?? string.Empty);
+
+            return View(model);
+        }
+    }
+}
